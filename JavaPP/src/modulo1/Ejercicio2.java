32fe83fc03fc581ff6ec398d7c40c6a45cd1ea94
P042406 <P042406@0042pc01.ccba.usr.bpba>
2021-10-09 17:16:04 -0300

    Original.

---
+++ JavaPP/src/modulo1/Ejercicio2.java
@@ -0,0 +1,14 @@
+package modulo1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Pato");
+		System.out.println("\n\nnombre\t\ttapelido\temail\tinterno");
+		System.out.println("\nPatricia\tPuyol\t\tpuyolpatricia@gmail.com\t19523");
+		System.out.println("\nMaría\tMartinez\t\tmmartinez@bpba.com.ar\t27835");
+		System.out.println("\nErica\tSimon\t\tsimone1956@gmail.com\t32856");
+
+	}
+
+}
