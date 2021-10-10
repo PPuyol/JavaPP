4b861962366f2fff6772826cd28bd4471a9a8abc
P042406 <P042406@0042pc01.ccba.usr.bpba>
2021-10-09 21:19:26 -0300

    modulo1.Ejercicio3

---
+++ JavaPP/src/modulo1/Ejercicio3.java
@@ -0,0 +1,17 @@
+package modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		System.out.println("Tecla de escape\t\tsignificado");
+		System.out.println("");
+		System.out.println("\\n\t\t\tsignifica nueva línea");
+		System.out.println("\\t\t\t\tsignifica un tab de espacio");
+		System.out.println("\\\"\t\t\tes para poner \" (comillas dobles) dentro del texto por ejemplo ");
+		System.out.println("\t\t\t\"Belencita\"");
+		System.out.println("\\\\\t\t\tse utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\\'\t\t\tse utiliza para las '(comillas simples) para escribir por ejemple 'Princesita'");
+		
+	}
+
+}
